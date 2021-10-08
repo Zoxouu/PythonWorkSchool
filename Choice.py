9773b1eaf5ba596677520cfa6efe9c6b117faec1
Zoxouu <74683259+Zoxouu@users.noreply.github.com>
2021-10-08 12:20:29 +0200

    Add files via upload

---
+++ Choice.py
@@ -0,0 +1,9 @@
+﻿# https://github.com/Zoxouu
+
+from random import choice
+
+liste1 = ['a','b','c','d','e','f','g','h']
+liste2 = ['1','2','3','4','5','6']
+
+for x in range(4) :
+    print(choice(liste1), choice(liste2), end="")

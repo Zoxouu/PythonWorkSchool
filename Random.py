cbd0f97667b215c6005ab19817cca1d69bb1acdd
Zoxouu <74683259+Zoxouu@users.noreply.github.com>
2021-10-08 11:49:40 +0200

    Add files via upload

---
+++ Random.py
@@ -0,0 +1,16 @@
+﻿# https://github.com/Zoxouu
+
+from random import randint
+
+
+# 50 dés a 6 faces
+for x in range(50):
+    print(randint(1,6), end=' ')
+    print(randint(1,6), end=' ')
+    print(randint(1,6), end=' ')
+
+
+# 200 dés a 8 faces
+for x in range(200):
+    print(randint(1,8), end=' ')
+    print(randint(1,8), end=' ')

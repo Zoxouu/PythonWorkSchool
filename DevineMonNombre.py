0be08b1879d54f006a36fe438c03b3161671f670
Zoxouu <74683259+Zoxouu@users.noreply.github.com>
2021-10-15 11:56:38 +0200

    Update DevineMonNombre.py

--- DevineMonNombre.py
+++ DevineMonNombre.py
@@ -4,22 +4,22 @@ from random import randint
 nbr_essais_max = 5
 nbr_essais = 1
 borne_sup = 30
-mon_nombre = randit(1,borne_sup)
+mon_nombre = randint(1,borne_sup)
 ton_nombre = 0
 
 print("J'ai choisis un nombre entre 1 et",borne_sup)
 print("A vous de le deviner en",nbr_essais_max,"tentatives au maximum !")
 
-while ton_nombre != mon_nombre and nbr_essais <= nbr_esais_max:
+while ton_nombre != mon_nombre and nbr_essais <= nbr_essais_max:
     print("Essai no ",nbr_essais)
-    ton_nombre = int(input(Votre proposition : ))
+    ton_nombre = int(input("Votre proposition : "))
     if ton_nombre < mon_nombre:
         print("Trop p'tit")
-        elif ton_nombre > mon_nombre:
+    elif ton_nombre > mon_nombre:
             print("Trop grand")
-            else:
-                print("Bravo ! Vous avez trouver ",mon_nombre,"en",nbr_essais,"essai(s)")
-                nbr_essais += 1
+    else:
+        print("Bravo ! Vous avez trouver ",mon_nombre,"en",nbr_essais,"essai(s)")
+    nbr_essais += 1
 
 if nbr_essais>nbr_essais_max and ton_nombre != mon_nombre :
     print("Désolé vous avez utilisé vos",nbr_essais_max,"essais en vain.")

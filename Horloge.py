a04947791b2d4f226ba250fa66bdfa05ee862c20
Zoxouu <74683259+Zoxouu@users.noreply.github.com>
2021-10-15 12:16:44 +0200

    Add files via upload

---
+++ Horloge.py
@@ -0,0 +1,10 @@
+﻿h=0
+while h<24:
+    m=0
+    while m<60:
+        s=0
+        while s<60:
+            print(h,"heure(s) ",m,"minutes(s) ",s,"seconde(s)")
+            s+=1
+        m+=1
+    h+=1

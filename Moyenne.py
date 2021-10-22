6aea90f55f2a52b1cb00c75f622c89b4d187c0b3
Zoxouu <74683259+Zoxouu@users.noreply.github.com>
2021-10-22 11:21:47 +0200

    Update Moyenne.py

--- Moyenne.py
+++ Moyenne.py
@@ -1,4 +1,8 @@
-﻿Val1=float(input("la,première valeur est : "))
+# https://github.com/Zoxouu
+
+Val1=float(input("la,première valeur est : "))
 Val2=float(input("La deuxième valeur est : "))
+
 Moyenne=(Val1+Val2)/2
+
 print("LA MOYENNE EST DE / ",Moyenne)
